c8bb286130a511beee91c64265a5943bfb743491
qwerty9927 <tanvo9927@gmail.com>
2023-05-24 15:03:32 +0700

    Section 2: Node.js Backend Architecture

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const morgan = require("morgan")
+const {default: helmet} = require("helmet")
+const compression = require("compression")
+const app = express()
+
+// middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// inti db
+
+// router
+
+app.get('/', (req, res, next) => {
+  res.status(200).send("Hello")
+})
+
+// handing errors
+
+module.exports = app
